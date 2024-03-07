a99e133b4580ef1ce3b83f5c420c1289a68c70bf
mfrautsc <marco.frautschi@stud.hslu.ch>
2024-03-07 20:57:48 +0100

    Initial Commit

---
+++ src/main/java/ch/swat/choosy/ChoosyApplication.java
@@ -0,0 +1,13 @@
+package ch.swat.choosy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoosyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChoosyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/swat/choosy/ChoosyApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.swat.choosy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoosyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
