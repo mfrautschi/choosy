3539a2690825c06dd3415b20d0740603fde760d6
mfrautsc <marco.frautschi@stud.hslu.ch>
2024-03-26 18:01:40 +0100

    fix: correctly implemented dependency injection via constructor & logged some debug-infos

--- src/main/java/ch/swat/choosy/controller/TeamGenerationController.java
+++ src/main/java/ch/swat/choosy/controller/TeamGenerationController.java
@@ -2,6 +2,8 @@ package ch.swat.choosy.controller;
 
 import ch.swat.choosy.bo.TeamVariant;
 import ch.swat.choosy.service.TeamGenerationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,20 +15,28 @@ import java.util.Map;
 @Controller
 public class TeamGenerationController {
     TeamGenerationService teamGenerationService;
+    private final Logger logger = LoggerFactory.getLogger(TeamGenerationController.class);
+
+    private TeamGenerationController(TeamGenerationService teamGenerationService){
+        this.teamGenerationService = teamGenerationService;
+    }
 
     @GetMapping(path="/")
     public String initialTeamGeneration(){
+        logger.debug("GET Request received");
         return "homeForm";
     }
 
     @PostMapping(path="/generate")
-    public String generateTeamVaraiation(@RequestParam String participants, @RequestParam String teamvariant, Model model){
+    public String generateTeamVaraiation(final @RequestParam String participants, final @RequestParam String teamvariant, final Model model){
+        logger.debug("POST Request received");
         Map<TeamVariant,Map<String, String>> teams;
         if(teamvariant.equals("TALL")){
             teams = teamGenerationService.generateAllPossibleTeams(participants);
         } else {
             teams = teamGenerationService.generatePossibleTeam(participants, TeamVariant.valueOf(teamvariant));
         }
+        teams.forEach((key, value) -> logger.debug(key + ":" + value));
         model.addAttribute("teams", teams);
         return "generationResult";
     }
