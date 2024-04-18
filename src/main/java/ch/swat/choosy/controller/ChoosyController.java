ad1decdb74b8019ea3f623871413603b6f473591
marco <marco.frautschi@bluewin.ch>
2024-04-18 18:10:24 +0200

    refactor: outsourced teamgeneration

---
+++ src/main/java/ch/swat/choosy/controller/ChoosyController.java
@@ -0,0 +1,24 @@
+package ch.swat.choosy.controller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
+
+@Controller
+public class ChoosyController {
+    private final Logger logger = LoggerFactory.getLogger(ChoosyController.class);
+
+    @GetMapping(path="/")
+    public void index(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        logger.debug("GET Request on index");
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/team/");
+        dispatcher.forward(request, response);
+    }
+}
--- src/main/java/ch/swat/choosy/controller/TeamController.java
+++ src/main/java/ch/swat/choosy/controller/TeamController.java
@@ -6,31 +6,30 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @Controller
-public class TeamGenerationController {
-    final TeamGenerationService teamGenerationService;
-    private final Logger logger = LoggerFactory.getLogger(TeamGenerationController.class);
+@RequestMapping(path="/team")
+public class TeamController {
+    private final Logger logger = LoggerFactory.getLogger(TeamController.class);
+    private final TeamGenerationService teamGenerationService;
 
-    public TeamGenerationController(TeamGenerationService teamGenerationService){
+    public TeamController(TeamGenerationService teamGenerationService) {
         this.teamGenerationService = teamGenerationService;
     }
 
     @GetMapping(path="/")
-    public String initialTeamGeneration(){
-        logger.debug("GET Request received");
+    public String initialTeam(){
+        logger.debug("GET Request received - initialTeam");
         return "homeForm";
     }
 
     @PostMapping(path="/generate")
     public String generateTeamVaraiation(final @RequestParam String participants, final @RequestParam String teamvariant, final Model model){
         logger.debug("POST Request received");
-        Map<TeamVariant,Map<String, String>> teams;
+        Map<TeamVariant, Map<String, String>> teams;
         if(teamvariant.equals("TALL")){
             teams = teamGenerationService.generateAllPossibleTeams(participants);
         } else {
