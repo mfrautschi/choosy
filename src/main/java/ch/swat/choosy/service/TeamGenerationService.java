823e57e1f5e69bc58f1abc7e7c9f1543d939046b
marco <marco.frautschi@bluewin.ch>
2024-04-23 16:37:45 +0200

    refactor: tried it with lambdas

--- src/main/java/ch/swat/choosy/service/TeamGenerationService.java
+++ src/main/java/ch/swat/choosy/service/TeamGenerationService.java
@@ -9,24 +9,26 @@ import java.util.concurrent.ThreadLocalRandom;
 @Service
 public class TeamGenerationService {
     public Map<TeamVariant, Map<String, String>> generateAllPossibleTeams(String participants) {
-        var variants = calculatePossibleTeamVariants(participants.split(";"));
+
         Map<TeamVariant, Map<String, String>> assignedTeams = new EnumMap<>(TeamVariant.class);
-        for (TeamVariant variant : variants) {
-            assignedTeams.put(variant, assignParticipantsToTeams(variant, participants.split(";")));
-        }
+        calculatePossibleTeamVariants(participants.split(";"))
+                .stream()
+                .parallel()
+                .forEach(v -> assignedTeams.put(v, assignParticipantsToTeams(v, participants.split(";"))));
+
         return assignedTeams;
     }
 
     private Map<String, String> assignParticipantsToTeams(TeamVariant variant, String[] participants) {
+
         Map<String, String> teams = new HashMap<>();
         List<String> part = new ArrayList<>(Arrays.stream(participants).toList());
         int anzTeams = part.size() / variant.size();
-
         for (int i = 1; i <= anzTeams; i++) {
             for (int c = 1; c <= variant.size(); c++) {
-                if (teams.get(String.valueOf(i))==null) {
+                if (teams.get(String.valueOf(i)) == null) {
                     teams.put(String.valueOf(i), part.remove(ThreadLocalRandom.current().nextInt(part.size())));
-                }else{
+                } else {
                     teams.put(String.valueOf(i), teams.get(String.valueOf(i)) + ";" + part.remove(ThreadLocalRandom.current().nextInt(part.size())));
                 }
             }
@@ -36,20 +38,16 @@ public class TeamGenerationService {
 
     public Map<TeamVariant, Map<String, String>> generatePossibleTeam(String participants, TeamVariant variant) {
         Map<TeamVariant, Map<String, String>> assignedTeams = new EnumMap<>(TeamVariant.class);
-        if(calculatePossibleTeamVariants(participants.split(";")).contains(variant)){
+        if (calculatePossibleTeamVariants(participants.split(";")).contains(variant)) {
             assignedTeams.put(variant, assignParticipantsToTeams(variant, participants.split(";")));
         }
         return assignedTeams;
     }
 
     public List<TeamVariant> calculatePossibleTeamVariants(String[] participants) {
-        List<TeamVariant> possibleTeamVariants = new LinkedList<>();
-
-        for (TeamVariant variant : TeamVariant.values()) {
-            if (participants.length % variant.size() == 0 && participants.length != variant.size()) {
-                possibleTeamVariants.add(variant);
-            }
-        }
-        return possibleTeamVariants;
+        return Arrays
+                .stream(TeamVariant.values())
+                .filter(v -> participants.length % v.size() == 0 && participants.length != v.size())
+                .toList();
     }
 }
